8b155a5a1d54dbeae475d0126f212c6987e74f1e
BiancoRocha <rafaelbianco1@hotmail.com>
2025-08-16 18:36:06 -0300

    Criado a rota produtos por categoria

---
+++ API/produtos-por-categoria.php
@@ -0,0 +1,22 @@
+<?php
+include("../conexao.php");
+
+$sql_produtos = "SELECT * FROM produtos_por_categoria_vw";
+$result = $conn->query($sql_produtos);
+
+$data = [];
+
+if($result && $result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+        $data[] =[ 
+            'categoria' => $row['categoria'],
+            'total' => $row['total'],
+        ];
+        
+    }
+}
+header('Content-Type: application/json; charset=UTF-8');
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, OPTIONS");
+
+echo json_encode($data);
