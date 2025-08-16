68e7b44b90bc0f72a1d60bf833b1d7b52cabf850
BiancoRocha <rafaelbianco1@hotmail.com>
2025-08-16 18:25:21 -0300

    Criado a rota Categoria mais cadastrada

---
+++ API/categoria-mais-cadastrada.php
@@ -0,0 +1,23 @@
+<?php
+include("../conexao.php");
+
+$sql_produtos = "SELECT * FROM categoria_interesse_vw";
+$result = $conn->query($sql_produtos);
+
+$data = [];
+
+if($result && $result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+        $data[] =[ 
+            'categoria' => $row['categoria'],
+            'quantidade' => $row['QTD'],
+            'ultima_url' => $row['ULTIMA_URL'],
+        ];
+        
+    }
+}
+header('Content-Type: application/json; charset=UTF-8');
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, OPTIONS");
+
+echo json_encode($data);
